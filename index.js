51217dec4e6e560b944559301cb5a4081dd819f2
Chris Salvato <csalvato@gmail.com>
2019-10-11 10:10:24 -0600

    Modifies code to spit out only the metrics we care about.

--- index.js
+++ index.js
@@ -3,28 +3,65 @@
 var Mortgage = require('./mortgage');
 var print = require('node-print');
 
-var amount = 96000;
-var interest = 0.03875;
-var years = 20;
-var afterMonths = 6;
+const currentAmount = 305763.75
+const originalAmount = 317925
 
-var mortgage = new Mortgage(amount, interest, years);
-var payment = mortgage.fixedMonthlyPayment();
-var remaining = mortgage.remainingLoanBalance(afterMonths);
-var amortTable = mortgage.amortizationTable();
-var amortList = amortTable.map(function (row, i) {
-  return {
-    'Month': i,
-    'Payment': row[0],
-    'Principal Paid': row[1],
-    'Interest Paid': row[2],
-    'Total Interest': row[3],
-    'Balance': row[4]
-  };
-});
+const scenarios = [
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 1500, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: 3500 },
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 0, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: 3500 },
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 1500, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: 2100 },
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 0, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: 2100 },
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 1500, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: 1540.82 },
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 0, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: 1540.82 },
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 1500, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: null },
+  { name: 'New 30 year', amount: currentAmount, rolledInFees: 0, interest: 3.6 / 100, years: 30, desiredMonthlyPayment: null },
+  { name: 'Original 30 year', amount: originalAmount, rolledInFees: 0, interest: 4.125 / 100, years: 30, desiredMonthlyPayment: 3500 },
+  { name: 'Original 30 year', amount: originalAmount, rolledInFees: 0, interest: 4.125 / 100, years: 30, desiredMonthlyPayment: 2100 },
+  { name: 'Original 30 year', amount: originalAmount, rolledInFees: 0, interest: 4.125 / 100, years: 30, desiredMonthlyPayment: null },
+]
 
-console.log('Fixed monthly payments: ' + payment);
-console.log('Remaining balance after ' + afterMonths + ' months: ' + remaining);
+let table = []
+scenarios.forEach((scenario) => {
+  const { name, amount, rolledInFees, interest, years, desiredMonthlyPayment } = scenario
+  var mortgage = new Mortgage(amount, rolledInFees, interest, years, desiredMonthlyPayment);
+  var payment = mortgage.fixedMonthlyPayment();
+  var monthOfPrincipalDominance = mortgage.monthOfPrincipalDominance()
+  var amortTable = mortgage.amortizationTable();
+  // var amortList = amortTable.map(function (row, i) {
+  //   return {
+  //     'Month': i + 1,
+  //     'Payment': row[0],
+  //     'Principal Paid': row[1],
+  //     'Interest Paid': row[2],
+  //     'Total Interest': row[3],
+  //     'Total Payment': row[4],
+  //     'Balance': row[5]
+  //   };
+  // });
 
-console.log('Amortization table:');
-print.pt(amortList);
+
+  const currencyFormat = (number) => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(number)
+
+  table.push({
+    'Name': name,
+    'Minimum Payment': currencyFormat(mortgage.minimumPayment()),
+    'Monthly Payment': currencyFormat(payment),
+    'Rate':  Math.round(((interest)*100000))/1000 + '%',
+    'Rolled In Fees': currencyFormat(rolledInFees),
+    'Total Payments': currencyFormat(amortTable[amortTable.length-1][4]),
+    'Total Interest': currencyFormat(amortTable[amortTable.length-1][3]),
+    'Full term': Math.round(((amortTable.length / 12)*100))/100 + ' years',
+    'Principal Dominance': Math.round(((monthOfPrincipalDominance / 12)*100))/100 + ' years'
+  })
+})
+
+// console.log('Minimum Payment: ', mortgage.minimumPayment());
+// console.log('Fixed monthly payments: ' + payment);
+// console.log('Total Payments:', amortTable[amortTable.length-1][4]);
+// console.log('Total Interest:', amortTable[amortTable.length-1][3]);
+// console.log('Full term in years:', amortTable.length / 12)
+// console.log('Time Until Principal Dominance', Math.round(((monthOfPrincipalDominance / 12)*100))/100)
+
+// console.log('Amortization table:');
+// print.pt(amortList);
+print.pt(table)
--- mortgage.js
+++ mortgage.js
@@ -2,17 +2,29 @@
 
 var money = require('./money');
 
-function Mortgage(amount, interest, years) {
-  this.amount = amount;
+function Mortgage(amount, rolledInFees, interest, years, desiredMonthlyPayment) {
+  this.amount = amount + rolledInFees;
   this.interest = interest / 12;
   this.months = years * 12;
+  this.extraPayment = 0
+  if(desiredMonthlyPayment) this.extraPayment = money.round(desiredMonthlyPayment - this.fixedMonthlyPayment());
 }
+
+Mortgage.prototype.minimumPayment = function () {
+  var a = this.amount;
+  var i = this.interest;
+  var n = this.months;
+  var payment = a * ((i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1))
+
+  return payment.toFixed(2);
+};
 
 Mortgage.prototype.fixedMonthlyPayment = function () {
   var a = this.amount;
   var i = this.interest;
   var n = this.months;
-  var payment = a * ((i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
+  var payment = a * ((i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1))
+  if (this.extraPayment) payment += this.extraPayment
 
   return payment.toFixed(2);
 };
@@ -27,6 +39,21 @@ Mortgage.prototype.remainingLoanBalance = function (afterMonths) {
   return balance.toFixed(2);
 };
 
+Mortgage.prototype.monthOfPrincipalDominance = function () {
+  var balance = this.amount;
+  var interest = this.interest;
+  var payment = this.fixedMonthlyPayment();
+  for (var i = 0; i < this.months; i++) {
+
+    // TODO: decimal rounding
+    var interestPaid = money.round(balance * interest);
+    var principalPaid = money.round(payment - interestPaid);
+    var minimumPrincipalPaid = money.round(principalPaid - this.extraPayment);
+    if(interestPaid < minimumPrincipalPaid) return i + 1;
+    balance = money.round(balance - principalPaid);
+  }
+}
+
 // Returns a table of the following: payment, principal paid, interest paid, total interest, balance.
 Mortgage.prototype.amortizationTable = function () {
   var balance = this.amount;
@@ -34,6 +61,7 @@ Mortgage.prototype.amortizationTable = function () {
   var payment = this.fixedMonthlyPayment();
   var totalInterestPaid = 0;
   var totalPrincipalPaid = 0;
+  var totalPayments = 0;
   var table = [];
 
   for (var i = 0; i < this.months; i++) {
@@ -41,16 +69,21 @@ Mortgage.prototype.amortizationTable = function () {
     // TODO: decimal rounding
     var interestPaid = money.round(balance * interest);
     var principalPaid = money.round(payment - interestPaid);
+    var totalPayment = money.round(principalPaid + interestPaid)
+    if(balance < totalPayment) principalPaid = money.round(balance - interestPaid)
 
     totalInterestPaid = money.round(totalInterestPaid + interestPaid);
     totalPrincipalPaid = money.round(totalPrincipalPaid + principalPaid);
+    totalPayments = money.round(totalInterestPaid + totalPrincipalPaid)
     balance = money.round(balance - principalPaid);
+    if(balance === 0) return table;
 
     table.push([
       payment,
       principalPaid,
       interestPaid,
       totalInterestPaid,
+      totalPayments,
       balance
     ]);
 
